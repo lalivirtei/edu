cce99e4662310c2ed7ea24dc8fb4dcdbe181d946
Denis <lalivirtei@gmail.com>
2023-02-16 08:52:13 +0300

    simple palindrome

---
+++ quests/palindrome.js
@@ -0,0 +1,14 @@
+function isPalindrome(str) {
+    let val = false;
+    let newStr = str.split('').reverse().join('');
+
+    if (newStr === str) {
+        val = true;
+    }
+
+    return val;
+}
+
+console.log(isPalindrome('тест')); // false
+console.log(isPalindrome('шалаш')); // true
+console.log(isPalindrome('112211')); // true
