8af0190e347d40184470678d3c8dff5fc5bb1c3a
Denis <lalivirtei@gmail.com>
2023-03-02 10:58:46 +0300

    a bit of homework

---
+++ arrayMethods/getWeekDay.js
@@ -0,0 +1,16 @@
+// let date = new Date(2012, 0, 3);  // 3 января 2012 года
+// console.log(getWeekDay(date));        // нужно вывести "ВТ"
+//
+// function getWeekDay(date) {
+//   let ruDays = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+//   return ruDays[date.getDay()];
+// }
+
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+console.log(getLocalDay(date));       // вторник, нужно показать 2
+
+function getLocalDay(date) {
+  let day = date.getDay();
+  if (day === 0) day = 7;
+  return day;
+}
