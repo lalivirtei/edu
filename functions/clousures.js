f0f5cde359a39e639d396e592ec0a227afd962f1
Denis <lalivirtei@gmail.com>
2023-03-04 23:25:42 +0300

    a bit of homework

---
+++ functions/clousures.js
@@ -0,0 +1,61 @@
+/* 1 */
+console.log( sum(1)(2) ); // = 3
+console.log( sum(5)(-1) ); // = 4
+
+function sum(num) {
+  return function (num2) {
+    return num + num2;
+  }
+}
+
+/* 2 */
+function inBetween(a, b) {
+  return function (item) {
+    return item >= a && item <= b
+  }
+}
+
+function inArray(arr) {
+  return function (item) {
+    return arr.includes(item);
+  }
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+console.log( arr.filter(inArray([1, 2, 10])) ); // 1,2
+
+/* 3 */
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+users.sort(byField('name'));
+console.log(users)
+users.sort(byField('age'));
+console.log(users)
+
+function byField(field) {
+  return (a, b) => a[field] > b[field] ? 1 : -1;
+}
+
+/* 4 */
+function makeArmy() {
+  let shooters = [];
+
+  for (let i = 0; i < 10; i++) {
+    let shooter = function () { // функция shooter
+      console.log(i); // должна выводить порядковый номер
+    };
+    shooters.push(shooter);
+  }
+
+  return shooters;
+}
+
+let army = makeArmy();
+
+army[0](); // у 0-го стрелка будет номер 10
+army[5](); // и у 5-го стрелка тоже будет номер 10
+// ... у всех стрелков будет номер 10, вместо 0, 1, 2, 3...
---
+++ functions/nfe.js
@@ -0,0 +1,39 @@
+// function makeCounter() {
+//
+//   function counter() {
+//     return counter.count++;
+//   }
+//
+//   counter.count = 0;
+//   counter.set = function (value) {
+//     this.count = value;
+//   }
+//   counter.decrease = function () {
+//     this.count--;
+//   }
+//
+//   return counter;
+// }
+// let counter = makeCounter();
+//
+// counter.set(109);
+// counter.decrease();
+// console.log(counter()); // 10
+//
+
+function sum(num) {
+  let current = num;
+
+  function inner(next) {
+    current += next;
+    return inner;
+  }
+
+  inner.toString = function () {
+    return current;
+  }
+
+  return inner;
+}
+
+console.log( sum(0)(1)(2)(3)(4)(5).toString()); // 15
