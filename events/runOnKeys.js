b912c1dd70b35563bd3b95834cc2a970a5c79669
Denis <lalivirtei@gmail.com>
2023-03-09 11:40:49 +0300

    a bit of homework

---
+++ events/runOnKeys.js
@@ -0,0 +1,10 @@
+// Создайте функцию runOnKeys(func, code1, code2, ... code_n), которая запускает func при одновременном
+// нажатии клавиш с кодами code1, code2, …, code_n.
+
+function runOnKeys(callback, ...args) {
+  let pressed = [];
+
+  document.addEventListener('keydown', () => {
+
+  })
+}
