73103f3ed9f2cc4b16a251465f2020c442957fdf
Denis <lalivirtei@gmail.com>
2023-04-14 16:52:17 +0300

    simplified

--- tasks/sumDigits.js
+++ tasks/sumDigits.js
@@ -2,10 +2,7 @@ function sumDigits(num) {
   return Math.abs(num)
       .toString()
       .split('')
-      .reduce((accum, current) => {
-        accum += Number(current);
-        return accum;
-      }, 0);
+      .reduce((accum, current) => accum + Number(current), 0);
 }
 
 console.log(sumDigits(99)); //18
--- tasks/sumNumbers.js
+++ tasks/sumNumbers.js
@@ -1,8 +1,5 @@
 function sumNumbers(...args) {
-  return args.reduce((accum, current) => {
-    accum += current;
-    return accum;
-  }, 0);
+  return args.reduce((accum, current) => accum + current, 0);
 }
 
 console.log(sumNumbers(1, 2, -2, 2)); // 3
